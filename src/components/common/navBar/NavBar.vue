2c6e8c8ab51a7e95b97a623d8ce3d68a297ddb91
chbw1227 <915518601@qq.com>
2020-04-05 19:40:27 +0800

    顶部导航栏组组件封装,home页面应用

---
+++ src/components/common/navBar/NavBar.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="nav-bar">
+		<!-- right -->
+		<div class="left">
+			<slot name="left"></slot>
+		</div>
+		<!-- center -->
+		<div class="center">
+			<slot name="center"></slot>
+		</div>
+		<!-- left -->
+		<div class="right">
+			<slot name="right"></slot>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'NavBar',
+	data() {
+		return {
+
+		}
+	}
+}
+</script>
+
+<style lang='scss' scoped>
+.nav-bar {
+	display: flex;
+	height: 44px;
+	line-height: 44px;
+	box-shadow: 0 1px 1px rgba(100, 100, 100, 0.1);
+}
+.left,
+.right {
+	width: 60px;
+	// background-color: pink;
+}
+.center {
+	flex: 1;
+	text-align: center;
+	// background-color: red;
+}
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,12 +1,20 @@
 <template>
-	<div class="">
-        home 页面
+	<div class="home">
+        <nav-bar class="home-nav">
+           <template v-slot:center>
+               <div>购物街</div>
+           </template>
+        </nav-bar>
 	</div>
 </template>
 
 <script>
+import navBar from 'components/common/navBar/NavBar'
 export default {
-	name: 'Home',
+    name: 'Home',
+    components:{
+        navBar
+    },
 	data() {
 		return {
 
@@ -16,4 +24,8 @@ export default {
 </script>
 
 <style lang='scss' scoped>
+.home-nav{
+    background-color: var(--color-tint);
+    color: white;
+}
 </style>
